5f257eed33ba73be248a0797b96a2d72cd9cf62f
HeavyByte <english.w.andrew@gmail.com>
2023-03-28 12:15:50 -0500

    fix unused logo in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ProjectShortcut from './components/project-icon/project-shortcut';
 import ProjectList from './components/project-list/project-list';
