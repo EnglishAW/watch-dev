0418d27321d518ffe607442747f6dee2eb5aa445
HeavyByte <english.w.andrew@gmail.com>
2023-03-29 12:54:18 -0500

    remove random dog import

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UpDownProject from './projects/up-down/up-down';
 import Home from './projects/home/home';
-import RandomDogProject from './projects/random-dog/random-dog';
+// import RandomDogProject from './projects/random-dog/random-dog';
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="up-down" element={<UpDownProject />} />
-          <Route path="random-dog" element={<RandomDogProject />} />
+          {/* <Route path="random-dog" element={<RandomDogProject />} /> */}
           {/* <Route path="*" element={<Home />} /> */}
       </Routes>
     </BrowserRouter>
